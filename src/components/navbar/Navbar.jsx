db20b18518c8c7faab1118bb958cc5007eefa7b4
Patrick M Shangala <patrickmshangala@Patricks-MacBook-Pro.local>
2023-07-18 10:17:37 +0200

    nav-links

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Link from 'next/link';
+
+
+const links = [
+    {
+      id: 1,
+      title: "Home",
+      url: "/",
+    },
+    {
+      id: 2,
+      title: "Portfolio",
+      url: "/portfolio",
+    },
+    {
+      id: 3,
+      title: "Blog",
+      url: "/blog",
+    },
+    {
+      id: 4,
+      title: "About",
+      url: "/about",
+    },
+    {
+      id: 5,
+      title: "Contact",
+      url: "/contact",
+    },
+    {
+      id: 6,
+      title: "Dashboard",
+      url: "/dashboard",
+    },
+  ];
+
+const Navbar = () => {
+  return (
+    <div>
+        <Link href="/">Logo</Link>
+        <div></div>
+    </div>
+  )
+}
+
+export default Navbar;
